5d10658fb8e1a8b05444891bddc4afb236e6fff7
nadia winy <nadiawiny@hotmail.com>
2023-09-28 17:09:29 -0300

    ...

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -7,6 +7,38 @@ async function listaVideos(){
     return conexaoConvertida
 }
 
+// foi criada toda estrutura de uma função assincrona que faz uma conexao com a API e cria uma requisição POST - cadastrar novas coisas na API 
+
+async function criaVideo(titulo, descricao, url, imagem){ // para declarar outros tipos de requisições 
+    const conexao = await fetch('Http://localhost: 300/videos', {
+        method: "POST", 
+        headers: {
+            'Content-type': 'application/json' // o Content-type serve para especificar que tipo de arquivo está sendo enviado ou recebido
+        },
+        body: JSON.stringify({  // vamos enviar o dados que a gente quer cadastrar nessa requisição
+            titulo: titulo, 
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    })
+    if(!conexao.ok){
+        throw new Error("Não foi possível enviar o video")
+    }
+
+    const conexaoConvertida = await conexao.json()
+    return conexaoConvertida
+}
+
+async function buscaVideo(termoDeBusca){
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`)
+    const conexaoConvertida = conexao.json()
+
+    return conexaoConvertida
+}
+
 export const conectaApi = {
-    listaVideos
+    listaVideos,
+    criaVideo,
+    buscaVideo
 }
--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -2,7 +2,7 @@ import {conectaApi} from "./conectaApi.js"
 
 const lista = document.querySelector('[data-lista]')// data atribusts serve pra individualizar elementos e serve para manipular o DOM dentro deles
 
-function constroiCard(titulo, descricao, url, imagem){
+export default function constroiCard(titulo, descricao, url, imagem){
     const video = document.createElement('li')
     video.className = 'videos__item'
     video.innerHTML = ` <iframe width="100%" height="72%" src="${url}"
@@ -19,10 +19,14 @@ function constroiCard(titulo, descricao, url, imagem){
 }
 
 async function listaVideos(){
-    const listaApi = await conectaApi.listaVideos()
-    listaApi.forEach(elemento => lista.appendChild(
-        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)
-    ))
+    try{ // colcoar mensagem de erros
+        const listaApi = await conectaApi.listaVideos()
+        listaApi.forEach(elemento => lista.appendChild(
+            constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)
+        ))
+    }catch{
+        lista.innerHTML = `<h2 class="mensagem__titulo">Não foi possível carregar a lista de videos</h2>`
+    }
 
 }
 
